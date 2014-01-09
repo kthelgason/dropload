6d0ff2ad5fb99a77172274d7b27c10059914212a
Kári Helgason <kthelgason@gmail.com>
2014-01-09 08:41:49 +0000

    basic modules

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
-    response.end();
-}).listen(8888);
+function start(route) {
+    http.createServer(function(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("request for " + pathname + " recieved");
+
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World!");
+        response.end();
+    }).listen(8888);
+    console.log("server has started");
+}
+
+exports.start = start;
