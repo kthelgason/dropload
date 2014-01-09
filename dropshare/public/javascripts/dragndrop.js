0e053b87044e789c20c4bca07c25a9ef9d8da8b5
Kári Helgason <kthelgason@gmail.com>
2014-01-09 11:16:40 +0000

    swiched to express to speed things up and finished first draft of the homepage

---
+++ dropshare/public/javascripts/dragndrop.js
@@ -0,0 +1,9 @@
+
+var dropbox = document.getElementById("dropbox");
+
+// event handlers
+
+dropbox.addEventListener("dragenter", dragEnter, false);
+dropbox.addEventListener("dragexit", dragExit, false);
+dropbox.addEventListener("dragover", dragOver, false);
+dropbox.addEventListener("drop", drop, false);
