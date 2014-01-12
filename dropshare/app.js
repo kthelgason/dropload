c29856dff7fd405abf38c77efbee4806e8c6b949
Kári Helgason <kthelgason@gmail.com>
2014-01-12 16:21:37 +0000

    file upload now works

--- dropshare/app.js
+++ dropshare/app.js
@@ -20,7 +20,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.multipart());
+app.use(express.multipart({uploadDir:'./uploads'}));
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
--- dropshare/routes/api.js
+++ dropshare/routes/api.js
@@ -1,5 +1,6 @@
 
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
 
 /*
  * POST file upload 
@@ -9,10 +10,13 @@ exports.upload = function(req, res){
     console.log(req.files);
 
     var tempPath = req.files.SelectedFile.path,
-        targetPath = path.resolve('./uploads/' + req.files.SelectedFile.name);
+        targetPath = req.files.SelectedFile.name;
 
     fs.rename(tempPath, targetPath, function(err) {
-        if (err) throw err;
+        if (err){ 
+            console.log(err);
+            throw err;
+        }
         console.log("Upload completed!");
     });
 };
